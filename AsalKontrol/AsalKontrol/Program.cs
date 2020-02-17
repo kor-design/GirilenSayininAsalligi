7a4ddde566b84c49d689d469839dea254bd79bfa
omer kor <omerkorr@gmail.com>
2020-02-17 15:36:48 +0300

    last

---
+++ AsalKontrol/AsalKontrol/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AsalKontrol
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
